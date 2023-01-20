82039fdcc2f7d43bd0f0280c4355a10012931343
Mindlestick <iorw0224@sju.ac.kr>
2023-01-20 20:08:56 +0900

    sort

--- 6.정렬/성적이 낮은 순서로 학생 출력하기.py
+++ 6.정렬/성적이 낮은 순서로 학생 출력하기.py
@@ -4,7 +4,7 @@ for i in range(n):
     d=input().split()
     arr.append((d[0],int(d[1])))
 
-arr=sorted(arr,key=lambda i: i[1])
+arr=sorted(arr,key=lambda x: x[1])
 
 for i in arr:
     print(i[0], end=' ')
