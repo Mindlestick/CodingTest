37fd641c279f85bcc865b25b93522e62a40594bd
Minji Park <iorw0224@gmail.com>
2023-01-27 00:36:26 +0900

    boj

--- Baekjoon/BOJ11899.py
+++ Baekjoon/BOJ11899.py
@@ -1,18 +1,17 @@
-w = input()
-l = {'(': 0, ')': 0}
-cnt = 0
-for i in w:
-    if i == '(':
-        if l[')'] == 0:
-            cnt += 1
-            l['('] += 1
-        else:
-            l[')'] -= 1
-            cnt -= 1
+#괄호 끼워넣기
+
+s=list(input())
+l=[]
+l.append(s[0])
+for i in range(1,len(s)):
+    if len(l)==0:
+        l.append(s[i])
     else:
-        if l['('] == 0:
-            cnt += 1
-        else:
-            l['('] -= 1
-            cnt -= 1
-print(cnt)
+        if l[-1]==s[i]:
+            l.append(s[i])
+        if l[-1]==')' and s[i]=='(':
+            l.append(s[i])
+        if l[-1]=='(' and s[i]==')':
+            l.pop()
+
+print(len(l))
