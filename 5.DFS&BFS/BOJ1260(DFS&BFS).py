6c4b202e5f37d27e0a6ab8730541a031129c75de
Minji Park <iorw0224@gmail.com>
2023-05-17 23:35:36 +0900

    구현

--- 5.DFS&BFS/BOJ1260(DFS&BFS).py
+++ 5.DFS&BFS/BOJ1260(DFS&BFS).py
@@ -1,50 +1,50 @@
-#DFS와 BFS
+# DFS와 BFS
 
 from collections import deque
 
 def dfs(curr):
-    #방문
-    visit[curr]=True
-    print(curr,end=' ')
-    
-    #방문 안한 노드 처리
+    # 방문
+    visit[curr] = True
+    print(curr, end=' ')
+
+    # 방문 안한 노드 처리
     for next in g[curr]:
-        if visit[next]==False:
+        if visit[next] == False:
             dfs(next)
 
 def bfs():
-    #방문
-    q=deque()
+    # 방문
+    q = deque()
     q.append(v)
-    visit[v]=True
-    
-    #큐 빌 때까지
+    visit[v] = True
+
+    # 큐 빌 때까지
     while len(q):
-        curr=q.popleft()
-        print(curr,end=' ')
+        curr = q.popleft()
+        print(curr, end=' ')
 
-        #방문 안한 인접노드 처리
+        # 방문 안한 인접노드 처리
         for next in g[curr]:
-            if visit[next]==False:
-	   	q.append(next) #큐에 넣고
-                	visit[next]=True #방문
-                
+            if visit[next] == False:
+                q.append(next)  # 큐에 넣고
+                visit[next] = True  # 방문
 
-n,m,v=map(int,input().split())
-g=[]
-g = [[] for i in range(n+1)]
+
+n, m, v = map(int, input().split())
+g = []
+g = [[] for i in range(n + 1)]
 
 for i in range(m):
-    a = list(map(int,input().split()))
-    #그래프 만들기
+    a = list(map(int, input().split()))
+    # 그래프 만들기
     g[a[0]].append(a[1])
     g[a[1]].append(a[0])
 
 for i in range(len(g)):
     g[i].sort()
 
-visit=[False]*10001
+visit = [False] * 10001
 dfs(v)
 print()
-visit=[False]*10001
+visit = [False] * 10001
 bfs()
